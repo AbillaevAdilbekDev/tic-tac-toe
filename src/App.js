d13caf395c1e9a867999407179b3da9d28b688e7
abillaevadilbek.com <151454581+AbillaevAdilbekDev@users.noreply.github.com>
2023-12-24 18:00:38 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import Game from './components/Navbar.js';
+function App() {
+  return (
+    <div className='App'>
+      <Game />
+    </div>
+  );
+}
+export default App;
